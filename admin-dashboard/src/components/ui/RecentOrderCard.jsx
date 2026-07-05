fe94facc738bd21e064cb29f6a53db9de861fe1d
KholoudFawzy <kholoudfawzy@gmail.com>
2026-07-06 01:21:31 +0300

    Improve recent orders UI

--- admin-dashboard/src/components/ui/RecentOrderCard.jsx
+++ admin-dashboard/src/components/ui/RecentOrderCard.jsx
@@ -6,6 +6,7 @@ const RecentOrderCard = ({order}) => {
     shipped: "bg-purple-100 text-purple-600",
     delivered: "bg-green-100 text-green-600",
     confirmed: "bg-green-100 text-green-600",
+    processing: "bg-gray-200 text-gray-500",
     };
 
     const statusClasses = statusColor[order.status];
@@ -20,7 +21,7 @@ const RecentOrderCard = ({order}) => {
     return (
         <div className="flex flex-col md:flex-row gap-3 bg-gray-50 justify-between md:items-center shadow-md mb-5 mx-1 p-5 border border-gray-200 rounded-2xl">
             <div className="flex flex-col gap-1">
-                <h4 className="font-semibold text-md text-gray-800">{order.accountType}</h4>
+                <h4 className="font-semibold text-sm text-gray-800">{order.accountType}</h4>
                 <div className="flex gap-2 text-gray-500 text-sm">
                     <span>{order.product}</span>
                     {/* {order.items[0].name} */}
