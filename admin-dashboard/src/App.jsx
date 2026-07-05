be7892ab448a1cc09b1826f920f2970d1b24c9c8
gitdigitfans <githubdigit@outlook.com>
2026-07-05 04:39:34 +0300

    commint-endmytask-yahya

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import Login from './login/Login.jsx';
+import { BrowserRouter, Route ,Routes } from 'react-router';
+
 function App() {
+
+
   return (
-    <>
-      <h1 className="text-4xl font-bold text-blue-600">Admin Dashboard</h1>
+    <> 
+    <BrowserRouter>
+    <Routes>
+       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+     
+    </BrowserRouter>
     </>
   );
 }
