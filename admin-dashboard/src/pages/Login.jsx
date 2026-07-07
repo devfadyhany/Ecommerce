5189d663e7c149af48015036d1848304cbf821c5
gitdigitfans <githubdigit@outlook.com>
2026-07-07 04:26:30 +0300

    Fix login page CSS

--- admin-dashboard/src/pages/Login.jsx
+++ admin-dashboard/src/pages/Login.jsx
@@ -4,6 +4,11 @@ import { useForm } from "react-hook-form";
 import api from "../api/axios";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { MdOutlinePassword } from "react-icons/md";
+import { FaEnvelope } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
+
+
 
 const emailRules = {
   required: "Email is required",
@@ -60,33 +65,30 @@ function Login() {
 
   return (
     <>
-      <div className="mx-auto w-[70%] gap-4 m-4 p-4 bg-gray-900 rounded-lg flex justify-between login-container">
-        <div className="flex-3 h-128 dleft rounded-lg shrink-1 hidden lg:block">
-          <h2 className="text-[25px] m-5 text-gray-900">Koda Commerce</h2>
-          <h1 className="text-[30px] mt-12 text-gray-900">
-            Manage Your Store Like a Pro
-          </h1>
-          <ul className="space-y-4 mt-20 text-gray-800 text-[16px]">
-            <li className="w-full bg-[#e4cc8d] rounded-xl p-4">
-              ✓ Product Management
-            </li>
-            <li className="w-full bg-[#e4cc8d] rounded-xl p-4">
-              ✓ Order Tracking
-            </li>
-            <li className="w-full bg-[#e4cc8d] rounded-xl p-4">
-              ✓ Customer Insights
-            </li>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-between login-container w-200  shadow-xl md:w-210">
+        <div className="space-y-4 flex-1 h-128 dleft rounded-l-lg p-2 shrink-1 hidden md:block d-login pr-5 pt-8">
+          <h2 className="text-[25px] ml-2  text-gray-900 flex items-center"><FiShoppingBag/> &nbsp; Koda Commerce</h2>
+          <h1 className="text-[30px] text-gray-900">Manage Your Store Like a Pro</h1>
+          <p className="text-[13px] pb-7">Control products, orders, users, carts and analytics from a modern dashboard experience</p>
+          <ul className="space-y-4 text-gray-800 text-[14px]">
+            <li className="w-full bg-[#fff2bc] rounded-xl p-3">✓ Product Management</li>
+            <li className="w-full bg-[#fff2bc] rounded-xl p-3">✓ Order Tracking</li>
+            <li className="w-full bg-[#fff2bc] rounded-xl p-3">✓ Customer Insights</li>
           </ul>
         </div>
 
-        <div className="lg:flex-4 w-full h-128  rounded-lg shadow-md  lg:p-5 p-2 form-container ">
-          <h2 className="text-4xl font-bold text-gray-800 text-center mt-13">
-            Login
-          </h2>
+        <div className="md:flex-1 w-full h-128  rounded-lg md:rounded-r-lg md:rounded-l-none  p-2 bg-red-900 form-container">
+          <div className="flex items-center flex-col mt-13 space-y-3">
+               <img src="../../" alt="log" />
+              <h2 className="text-3xl font-bold text-gray-800 text-center">Welcome Back</h2>
+              <p>Sign in to your admin dashboard</p>
+          </div>
+          
           <form
             onSubmit={handleSubmit(handleLogin)}
             className={
-              "relative space-y-7 flex flex-col w-full mt-13  shadow-4xl form-login"
+              "relative space-y-7 flex flex-col w-full mt-10 p-5  shadow-4xl form-login"
             }
           >
             <div className="relative">
@@ -94,8 +96,9 @@ function Login() {
                 type="text"
                 placeholder="enter your email"
                 {...register("email", emailRules)}
-                className="rounded-md py-2 px-5 border border-gray-500 focus:outline-none focus:ring-1 focus:ring-yellow-500 w-full "
+                className="rounded-md py-2 px-3 border border-gray-500 focus:outline-none focus:ring-1 focus:ring-yellow-500 w-full pl-10"
               />
+              <p className="absolute text-2xl top-2 left-2"><MdOutlinePassword/></p>
               <p
                 className={`${
                   isempty
@@ -106,7 +109,7 @@ function Login() {
                 enter your email
               </p>
               {errors.email && (
-                <p className="text-[#f7872c] text-xs ml-2 mt-1">
+                <p className="text-[#ff3700] text-xs ml-2 mt-1">
                   {errors.email.message}
                 </p>
               )}
@@ -117,8 +120,9 @@ function Login() {
                 type={passwordVisible ? "text" : "password"}
                 placeholder="enter your password"
                 {...register("password", passwordRules)}
-                className="rounded-md py-2 px-4 focus:outline-none focus:ring-1 focus:ring-yellow-500 w-full border border-gray-500"
+                className="rounded-md py-2 px-3 focus:outline-none focus:ring-1 focus:ring-yellow-500 w-full border border-gray-500 pl-10"
               />
+              <p className="absolute text-xl top-[12px] left-[13px]"><FaEnvelope /></p>
               <p
                 className={`${
                   isempty2
@@ -129,7 +133,7 @@ function Login() {
                 enter your password
               </p>
               {errors.password && (
-                <p className="text-[#f7872c] text-xs ml-2 -mt-1">
+                <p className="text-[#ff3700] text-xs ml-2 -mt-1">
                   {errors.password.message}
                 </p>
               )}
@@ -143,8 +147,6 @@ function Login() {
                 <p className=" text-gray-800 text-[12px]">Show Password</p>
               </div>
             </div>
-
-            {/* زر الإرسال */}
             <button
               disabled={loading}
               type="submit"
@@ -154,9 +156,14 @@ function Login() {
             >
               {loading ? "Logging in..." : "Login"}
             </button>
+            
+            
           </form>
         </div>
       </div>
+    </div>
+                
+      
     </>
   );
 }
