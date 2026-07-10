9e93a192f20917aca925f5ceb44c4255623f94e5
gitdigitfans <githubdigit@outlook.com>
2026-07-11 02:21:59 +0300

    تصور مبداي لاعدادات الموقع

--- admin-dashboard/src/pages/Settings.jsx
+++ admin-dashboard/src/pages/Settings.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 function Settings() {
+const [preferences  , setpreferences ] = useState (false) 
+ 
+
   return (
-    <div className="p-6 text-3xl font-bold">
-      Settings
+    
+    <>
+    <div className="p-5">
+
+    <div className="w-ful h-35 bg-yellow-200 rounded-xl">
+
+         </div>
+         <div className="mt-10 flex gap-3">
+        <button className="flex-1 bg-red-300 p-2 rounded-xl" onClick={()=>{setpreferences (true)}}>preferences </button>
+        <button className="flex-1 bg-red-300 p-2 rounded-xl" >theme</button>
+
+        <div className={`absolute w-300 h-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-yellow-300/70 rounded-2xl p-5 ${preferences  ? 'file-sh':`file-h`}`}>
+        <div className="h-80"></div>
+        <hr className="w-[70%] m-auto my-2"/>
+        <button className="px-3 py-1 bg-yellow-200" onClick={()=>{setpreferences (false)}}>Closes</button>
+        </div>
+
+         
+         </div>
+
+
     </div>
+         
+    </>
   );
 }
 
