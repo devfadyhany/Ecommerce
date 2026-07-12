505f62e8501bf8990bd00837d51176d850647153
samaa ehab <samaaehab01@gmail.com>
2026-07-12 22:02:22 +0300

    product page

--- admin-dashboard/src/pages/Products.jsx
+++ admin-dashboard/src/pages/Products.jsx
@@ -1,7 +1,14 @@
+import ProductsHeader from '../components/ui/ProductsHeader'
+import ProductStats from '../components/ui/ProductStats'
 function Products() {
   return (
-    <div className="p-6 text-3xl font-bold">
-      Products
+    <div>
+    <div className="p-8 ">
+      <ProductsHeader />
+    </div>
+    <div className="p-8">
+      <ProductStats />
+    </div>
     </div>
   );
 }
